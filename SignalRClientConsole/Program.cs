0cafa46aa4aa13523a75f48a505ed6326f124dba
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2014-07-22 09:51:42 +0700

    initial commit

---
+++ SignalRClientConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hubConnection = new HubConnection("http://localhost:8080/signalr");
+            var chat = hubConnection.CreateHubProxy("MyHub");
+            chat.On<string, string>(
+                "addMessage",
+                (name, message) => { Console.WriteLine("{0} : {1}", name, message); }
+            );
+            hubConnection.Start().Wait();
+
+            chat.Invoke("Notify", "Client Console", hubConnection.ConnectionId);
+
+            string chatMessage = null;
+
+            while ((chatMessage = Console.ReadLine()) != null)
+            {
+                chat.Invoke("Send", "Client Console", chatMessage).Wait();
+            }
+        }
+    }
+}
