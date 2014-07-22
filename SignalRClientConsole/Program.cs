316bf3bc83749642569953042c054011f41f24b8
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2014-07-22 13:34:58 +0700

    add instruction

--- SignalRClientConsole/Program.cs
+++ SignalRClientConsole/Program.cs
@@ -11,21 +11,35 @@ namespace SignalRClientConsole
     {
         static void Main(string[] args)
         {
+            // create hub connection to specified url
             var hubConnection = new HubConnection("http://localhost:8080/signalr");
+            // create hub proxy to communicate with one of specified hub
             var chat = hubConnection.CreateHubProxy("MyHub");
+            // register event and callback
             chat.On<string, string>(
                 "addMessage",
                 (name, message) => { Console.WriteLine("{0} : {1}", name, message); }
             );
+            // start create connection
             hubConnection.Start().Wait();
 
-            chat.Invoke("Notify", "Client Console", hubConnection.ConnectionId);
+            Console.WriteLine("Connection to server established");
 
+            string displayName = null;
             string chatMessage = null;
 
+            Console.Write("Display Name: ");
+            displayName = Console.ReadLine();
+
+            // adding the connection ID to the client list and notifying other clients about the arrival of this client
+            chat.Invoke("Notify", displayName, hubConnection.ConnectionId);
+
+            Console.Write("Write your message: ");
             while ((chatMessage = Console.ReadLine()) != null)
             {
-                chat.Invoke("Send", "Client Console", chatMessage).Wait();
+                // send the message
+                chat.Invoke("Send", displayName, chatMessage).Wait();
+                Console.Write("Write your message: ");
             }
         }
     }
